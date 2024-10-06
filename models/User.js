e3c1472ddffea82f72b2c09b7702fd00f119b5b2
SanthoshD123 <santhosh.damu78@gmail.com>
2024-10-06 17:23:11 +0530

    Define User schema and methods for MongoDB

---
+++ models/User.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
+    },
+    phone: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    phoneOtp: String,
+    phoneOtpExpires: Date,
+    isPhoneVerified: {
+      type: Boolean,
+      default: false,
+    },
+    profile: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
